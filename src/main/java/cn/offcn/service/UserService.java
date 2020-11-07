80bf52e91b91c4ed8196c232c4d58dc6cc50fab2
xiaozongshuai <2213731152@qq.com>
2020-11-07 09:30:56 +0800

    v1.0.0:提交代码测试

---
+++ src/main/java/cn/offcn/service/UserService.java
@@ -0,0 +1,5 @@
+package cn.offcn.service;
+
+public interface UserService {
+    public String getName();
+}
---
+++ src/main/java/cn/offcn/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package cn.offcn.service.impl;
+
+import cn.offcn.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public String getName() {
+        return "加油吧，肖帅！";
+    }
+}
