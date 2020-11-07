d4a70919b4ffee0e06f59501d60994914e3df1c8
xiaozongshuai <2213731152@qq.com>
2020-11-07 09:47:01 +0800

    v1.0.1:添加一个类，修改了代码

--- src/main/java/cn/offcn/service/impl/UserServiceImpl.java
+++ src/main/java/cn/offcn/service/impl/UserServiceImpl.java
@@ -5,6 +5,6 @@ import cn.offcn.service.UserService;
 public class UserServiceImpl implements UserService {
     @Override
     public String getName() {
-        return "加油吧，肖帅！";
+        return "加油吧，肖帅！123";
     }
 }
---
+++ src/main/java/cn/offcn/service/impl/hello.java
@@ -0,0 +1,7 @@
+package cn.offcn.service.impl;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
